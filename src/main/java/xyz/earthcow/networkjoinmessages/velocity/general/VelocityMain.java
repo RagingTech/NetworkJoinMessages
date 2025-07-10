c9727638f019b7924064b18bcd3155cbf2fbb761
EarthCow <56940983+EarthCow@users.noreply.github.com>
2025-07-10 10:44:55 -0400

    Bump snapshot 3 - stable MiniPlaceholders & PAPI

--- src/main/java/xyz/earthcow/networkjoinmessages/velocity/general/VelocityMain.java
+++ src/main/java/xyz/earthcow/networkjoinmessages/velocity/general/VelocityMain.java
@@ -27,7 +27,7 @@ import java.util.stream.Collectors;
 @Plugin(
     id = "networkjoinmessages",
     name = "NetworkJoinMessages",
-    version = "2.3.0-SNAPSHOT-2",
+    version = "2.3.0-SNAPSHOT-3",
     url = "https://github.com/RagingTech/NetworkJoinMessages",
     description = "A plugin handling join, leave and switch messages for proxy servers.",
     authors = { "EarthCow" },
