b12fe4f789d3b99d2c01a7172a5df9d336f80d15
EarthCow <56940983+EarthCow@users.noreply.github.com>
2025-07-11 14:52:32 -0400

    usercache.json import command logic

--- src/main/java/xyz/earthcow/networkjoinmessages/common/util/H2PlayerJoinTracker.java
+++ src/main/java/xyz/earthcow/networkjoinmessages/common/util/H2PlayerJoinTracker.java
@@ -1,7 +1,14 @@
 package xyz.earthcow.networkjoinmessages.common.util;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import xyz.earthcow.networkjoinmessages.common.general.NetworkJoinMessagesCore;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.*;
 import java.util.Enumeration;
 import java.util.UUID;
@@ -75,6 +82,29 @@ public class H2PlayerJoinTracker implements AutoCloseable {
             NetworkJoinMessagesCore.getInstance().getPlugin().getCoreLogger().severe(
                 "SQL Failure: Failed to mark player joined for player " + playerName);
         }
+    }
+
+    public boolean addUsersFromUserCache(String userCacheStr) {
+        Path userCachePath = Paths.get(userCacheStr);
+        if (!Files.exists(userCachePath)) {
+            return false;
+        }
+
+        try {
+            String json = new String(Files.readAllBytes(userCachePath));
+            JsonArray entries = JsonParser.parseString(json).getAsJsonArray();
+
+            for (JsonElement entry : entries) {
+                JsonObject obj = entry.getAsJsonObject();
+                String username = obj.get("name").getAsString();
+                UUID uuid = UUID.fromString(obj.get("uuid").getAsString());
+
+                markAsJoined(uuid, username);
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 
     private static void registerDriverIfNeeded() {
