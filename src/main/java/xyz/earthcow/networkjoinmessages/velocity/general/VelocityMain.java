635a70a3daf27a9f0cef8298851c67aea988fcaf
EarthCow <56940983+EarthCow@users.noreply.github.com>
2025-08-27 17:06:47 -0400

    Bumps to dev version

--- src/main/java/xyz/earthcow/networkjoinmessages/velocity/general/VelocityMain.java
+++ src/main/java/xyz/earthcow/networkjoinmessages/velocity/general/VelocityMain.java
@@ -32,7 +32,7 @@ import java.util.stream.Collectors;
 @Plugin(
     id = "networkjoinmessages",
     name = "NetworkJoinMessages",
-    version = "2.3.2",
+    version = "2.3.2^dev",
     url = "https://github.com/RagingTech/NetworkJoinMessages",
     description = "A plugin handling join, leave and switch messages for proxy servers.",
     authors = { "EarthCow" },
