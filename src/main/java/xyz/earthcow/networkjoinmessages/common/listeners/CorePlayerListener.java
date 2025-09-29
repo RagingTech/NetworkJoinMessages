94ec6e6f215989519d3f66136376e58f27f95fd9
EarthCow <56940983+EarthCow@users.noreply.github.com>
2025-09-28 23:46:26 -0400

    Silent event checking debug info

--- src/main/java/xyz/earthcow/networkjoinmessages/common/listeners/CorePlayerListener.java
+++ src/main/java/xyz/earthcow/networkjoinmessages/common/listeners/CorePlayerListener.java
@@ -52,6 +52,20 @@ public class CorePlayerListener {
     private boolean isSilentEvent(@NotNull CorePlayer player) {
         // Event is silent if, the player has a silent message state OR
         // premiumVanish is present, the treat vanished players as silent option is true, and the player is vanished
+        plugin.getCoreLogger().debug("Checking if the event for player " + player + " should been silent:");
+        plugin.getCoreLogger().debug(String.format(
+                "silent message state: %s, SayanVanish hook is NOT null: %s, SVTreatVanishedPlayersAsSilent: %s, " +
+                "SayanVanish player is vanished: %s, PremiumVanish hook is NOT null: %s, " +
+                "PVTreatVanishedPlayersAsSilent: %s, PremiumVanish player is vanished: %s"
+        ,
+            storage.getSilentMessageState(player),
+            sayanVanishHook != null,
+            storage.isSVTreatVanishedPlayersAsSilent(),
+            sayanVanishHook != null ? sayanVanishHook.isVanished(player) : "NA",
+            premiumVanish != null,
+            storage.isPVTreatVanishedPlayersAsSilent(),
+            premiumVanish != null ? premiumVanish.isVanished(player.getUniqueId()) : "NA"
+        ));
         return storage.getSilentMessageState(player) ||
                 (sayanVanishHook != null && storage.isSVTreatVanishedPlayersAsSilent() && sayanVanishHook.isVanished(player))
                 ||
