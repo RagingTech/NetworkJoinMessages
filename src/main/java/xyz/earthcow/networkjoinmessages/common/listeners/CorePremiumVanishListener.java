56435493359f84b281928b243d71604584f8130c
EarthCow <56940983+EarthCow@users.noreply.github.com>
2026-01-23 14:19:11 -0500

    Consistent if statements for PV listeners

--- src/main/java/xyz/earthcow/networkjoinmessages/common/listeners/CorePremiumVanishListener.java
+++ src/main/java/xyz/earthcow/networkjoinmessages/common/listeners/CorePremiumVanishListener.java
@@ -18,12 +18,13 @@ public class CorePremiumVanishListener {
     }
 
     public void handlePremiumVanishShow(@NotNull CorePlayer p) {
-        if (p.getPremiumVanishHidden()) {
-            logger.debug("Setting PremiumVanishHidden to FALSE for player " + p.getName());
-            p.setPremiumVanishHidden(false);
-            if (storage.isPVSpoofJoinMessageOnShow()) {
-                spoofManager.spoofJoin(p);
-            }
+        if (!p.getPremiumVanishHidden()) {
+            return;
+        }
+        logger.debug("Setting PremiumVanishHidden to FALSE for player " + p.getName());
+        p.setPremiumVanishHidden(false);
+        if (storage.isPVSpoofJoinMessageOnShow()) {
+            spoofManager.spoofJoin(p);
         }
     }
 
