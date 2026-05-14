8f254a4b25d0158bc578b57dc990bc4a7f092e15
EarthCow <56940983+EarthCow@users.noreply.github.com>
2026-05-14 14:05:40 -0400

    Makes CorePlayer an abstract class & uses lombok
    As a result of using Lombok, `setDisconnecting` now requires a boolean argument and `getPremiumVanishHidden` was renamed to `isPremiumVanishHidden`. These changes are good for consistency and clarity anyway.

--- src/main/java/xyz/earthcow/networkjoinmessages/common/abstraction/CorePlayer.java
+++ src/main/java/xyz/earthcow/networkjoinmessages/common/abstraction/CorePlayer.java
@@ -1,40 +1,34 @@
 package xyz.earthcow.networkjoinmessages.common.abstraction;
 
+import lombok.Getter;
+import lombok.Setter;
 import net.kyori.adventure.audience.Audience;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
 
-public interface CorePlayer extends CoreCommandSender {
+@Getter @Setter
+public abstract class CorePlayer implements CoreCommandSender {
+    // Fields
+    private CoreBackendServer lastKnownConnectedServer;
+    private boolean disconnecting = false;
+    private String cachedLeaveMessage;
+    private boolean premiumVanishHidden = false;
+    private int premiumVanishUseLevel = 0;
+    private int premiumVanishSeeLevel = 0;
+
+    public CorePlayer(CoreBackendServer lastKnownConnectedServer) {
+        this.lastKnownConnectedServer = lastKnownConnectedServer;
+    }
+
+    // Abstract
     @NotNull
-    UUID getUniqueId();
-
-    int getConnectionIdentity();
-
+    public abstract UUID getUniqueId();
+    public abstract int getConnectionIdentity();
     @Nullable
-    CoreBackendServer getCurrentServer();
-
-    @Nullable
-    CoreBackendServer getLastKnownConnectedServer();
-
-    void setLastKnownConnectedServer(CoreBackendServer server);
-
+    public abstract CoreBackendServer getCurrentServer();
     @NotNull
-    Audience getAudience();
-
-    boolean isInLimbo();
-
-    String getCachedLeaveMessage();
-    void setCachedLeaveMessage(String cachedLeaveMessage);
-
-    boolean isDisconnecting();
-    void setDisconnecting();
-
-    boolean getPremiumVanishHidden();
-    void setPremiumVanishHidden(boolean premiumVanishHidden);
-    int getPremiumVanishUseLevel();
-    void setPremiumVanishUseLevel(int level);
-    int getPremiumVanishSeeLevel();
-    void setPremiumVanishSeeLevel(int level);
+    public abstract Audience getAudience();
+    public abstract boolean isInLimbo();
 }
